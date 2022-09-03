a1f9eb4d0c550a5610db37236eed716463d18623
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2022-09-03 19:52:59 +0200

    Import scripts from modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
+import showHideSelectList from "./showHideSelectList";
+import displayCountries from "./displayCountries";
+
+displayCountries();
+
 let theme = localStorage.getItem("theme");
 if (theme === "dark") document.body.classList.add("dark");
 else document.body.classList.add("light");
 
 const themeBtn = document.querySelector(".header__changeThemeBtn");
+const filterSelect = document.querySelector(".field__filterSelect");
+
+filterSelect.addEventListener("click", showHideSelectList);
+
 themeBtn.addEventListener("click", () => {
   if (theme === "dark") {
     document.body.classList.remove("dark");
@@ -15,37 +24,3 @@ themeBtn.addEventListener("click", () => {
   }
   localStorage.setItem("theme", theme);
 });
-const filterSelect = document.querySelector(".field__filterSelect");
-filterSelect.addEventListener("click", () => {
-  const selectList = document.querySelector(".field__filterUl");
-  const selectOption = document.querySelectorAll(".field__filterBtn");
-  selectList.classList.toggle("field__filterUl--hide");
-
-  selectOption.forEach((btn) =>
-    btn.addEventListener("click", (e) => {
-      filterSelect.textContent = e.target.textContent;
-    })
-  );
-
-  window.addEventListener("click", (e) => {
-    if (
-      !e.target.classList.contains("field__filterUl") &&
-      !e.target.classList.contains("field__filterSelect")
-    ) {
-      selectList.classList.add("field__filterUl--hide");
-    }
-  });
-});
-
-// ??????????????
-// const countryCard = document.querySelectorAll(".country__flag");
-// countryCard.forEach((card) => {
-//   card.addEventListener("click", (e) => {
-//     console.log(e.target);
-//     const homeContent = document.querySelector(".main__homeContent");
-//     const details = document.querySelector(".main__details");
-
-//     homeContent.classList.add("main__homeContent--hide");
-//     details.classList.remove("main__details--hide");
-//   });
-// });
