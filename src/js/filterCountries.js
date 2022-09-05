fe1b5e6332e5216dd51a1d993d42fecd8adbdd20
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2022-09-05 12:59:04 +0200

    Fix filtering mechanism

--- src/js/filterCountries.js
+++ src/js/filterCountries.js
@@ -1,69 +1,63 @@
-export default function filterCountries(e) {
+export default function filterCountries(btnContent, inputValue) {
   const countryCards = document.querySelectorAll(".country");
-  //countryCards.forEach((card) => card.classList.remove("country--hide"));
-  let filterSelectValue;
-  let filterInputValue;
-
-  if (e.target.classList.contains("field__filterSelect")) {
-    filterBySelectList();
+  if (btnContent === "" && inputValue === "") {
+    countryCards.forEach((card) => card.classList.remove("country--hide"));
+  } else if (btnContent === "" && inputValue !== "") {
+    filterByInput(inputValue);
+  } else if (btnContent !== "" && inputValue === "") {
+    filterBySelectList(btnContent);
   } else {
-    filterByInput();
+    filterBySelectAndInput(btnContent, inputValue);
   }
 
-  function filterBySelectList() {
-    const selectList = document.querySelector(".field__filterUl");
-    const selectOption = document.querySelectorAll(".field__filterBtn");
-    const filterSelect = document.querySelector(".field__filterSelect");
-    selectList.classList.toggle("field__filterUl--hide");
-
-    selectOption.forEach((btn) =>
-      btn.addEventListener("click", (e) => {
-        filterSelect.textContent = e.target.textContent;
-        countryCards.forEach((card) => {
-          card.classList.remove("country--hide");
-          let btnContent = btn.textContent;
-          const countryCardRegionInfo =
-            card.childNodes[1].childNodes[2].childNodes[1].textContent;
-          btnContent === "America" ? (btnContent = "Americas") : btnContent;
-          if (countryCardRegionInfo !== btnContent) {
-            card.classList.add("country--hide");
-          }
-          filterSelectValue = btnContent;
-        });
-      })
-    );
-
-    window.addEventListener("click", (e) => {
-      if (
-        !e.target.classList.contains("field__filterUl") &&
-        !e.target.classList.contains("field__filterSelect")
-      ) {
-        selectList.classList.add("field__filterUl--hide");
-      }
+  function filterBySelectList(btnContent) {
+    countryCards.forEach((card) => {
+      const countryCardRegionInfo =
+        card.childNodes[1].childNodes[2].childNodes[1].textContent;
+      btnContent === "America" ? (btnContent = "Americas") : btnContent;
+      if (countryCardRegionInfo !== btnContent) {
+        card.classList.add("country--hide");
+      } else card.classList.remove("country--hide");
     });
   }
 
-  function filterByInput() {
-    const searchInput = document.querySelector(".field__searchInput");
+  function filterByInput(inputValue) {
     countryCards.forEach((card) => {
-      card.classList.remove("country--hide");
-      let searchInputContent = searchInput.value;
       const countryCardName = card.childNodes[1].childNodes[0].textContent;
       let flag = true;
       for (let i = 0; i <= countryCardName.length; i++) {
         if (
-          searchInputContent.toLocaleLowerCase() ===
+          inputValue.toLocaleLowerCase() ===
           countryCardName.toLocaleLowerCase().slice(0, i)
         ) {
           flag = false;
         }
       }
-
       if (flag) {
         card.classList.add("country--hide");
       }
+    });
+  }
 
-      filterInputValue = searchInputContent.toLocaleLowerCase();
+  function filterBySelectAndInput(btnContent, inputValue) {
+    countryCards.forEach((card) => {
+      const countryCardRegionInfo =
+        card.childNodes[1].childNodes[2].childNodes[1].textContent;
+      const countryCardName = card.childNodes[1].childNodes[0].textContent;
+      btnContent === "America" ? (btnContent = "Americas") : btnContent;
+
+      let flag = true;
+      for (let i = 0; i <= countryCardName.length; i++) {
+        if (
+          inputValue.toLocaleLowerCase() ===
+          countryCardName.toLocaleLowerCase().slice(0, i)
+        ) {
+          flag = false;
+        }
+      }
+      if (!flag && countryCardRegionInfo === btnContent) {
+        card.classList.remove("country--hide");
+      } else card.classList.add("country--hide");
     });
   }
 }
