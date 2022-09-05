6d4408e30ca68f1775cd4adcb55383f9b2d50b32
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2022-09-05 20:34:58 +0200

    Add error handling to getCountriesData function

--- src/js/getCountriesData.js
+++ src/js/getCountriesData.js
@@ -1,6 +1,10 @@
 export default async function getCountriesData() {
-  const url = "https://restcountries.com/v3.1/all";
-  const data = await fetch(url);
-  const response = await data.json();
-  return await response;
+  try {
+    const url = "https://restcountries.com/v3.1/all";
+    const data = await fetch(url);
+    const response = await data.json();
+    return await response;
+  } catch (e) {
+    console.error(e);
+  }
 }
